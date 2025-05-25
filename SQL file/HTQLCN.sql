f637fe1a561bca7921b7e32e1d2a2225dcdbe023
tuancaycu2004 <nh0ctuan600@gmail.com>
2025-05-25 19:20:22 +0700

    hoho

---
+++ SQL file/HTQLCN.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE HeThongQuanLyChanNuoi
+GO
+USE HeThongQuanLyChanNuoi
+GO
+
+-- 1. Bảng Tài khoản
+CREATE TABLE TaiKhoan (
+    TenDangNhap NVARCHAR(50) PRIMARY KEY,
+	TenHienThi NVARCHAR(50) NOT NULL DEFAULT 'NotNamedUser',
+    MatKhau VARCHAR(255) NOT NULL DEFAULT N'0',
+    Email NVARCHAR(100) UNIQUE NOT NULL,
+    LoaiTaiKhoan INT DEFAULT 0 --1: admin && 0: user
+);
+
+-- 2. Bảng Người Dùng
+CREATE TABLE NguoiDung (
+    IDNguoiDung VARCHAR(10) NOT NULL PRIMARY KEY,
+    HoTen NVARCHAR(100) NOT NULL DEFAULT N'Chưa nhập tên',
+	gioitinh NVARCHAR(30) NOT NULL CHECK (gioitinh IN(N'Nam',N'Nữ')),
+	NgaySinh DATE NOT NULL,
+	CCCD VARCHAR(50) NOT NULL,
+    Diachi NVARCHAR(100) NOT NULL DEFAULT N'Chưa nhập',
+    TenDangNhap NVARCHAR(50) NOT NULL,
+	CONSTRAINT FK_NguoiDung_TaiKhoan FOREIGN KEY (TenDangNhap) REFERENCES TaiKhoan(TenDangNhap),
+);
+
+-- 3. Bảng Vật nuôi
+CREATE TABLE VatNuoi (
+    IDVatNuoi VARCHAR(10) NOT NULL PRIMARY KEY,
+    loai NVARCHAR(100) NOT NULL DEFAULT N'Chưa nhập',
+	tenGiong NVARCHAR(100) NOT NULL,
+	tenChuong NVARCHAR(30) NOT NULL DEFAULT N'A1',
+    gioitinh NVARCHAR(30) NOT NULL DEFAULT N'Đực' CHECK (gioitinh IN(N'Cái',N'Đực')), --Cái || Đực
+    ngaySinh DATE NOT NULL,
+    canNang FLOAT NOT NULL CHECK (canNang > 0),
+	IDNguoiDung VARCHAR(10) NULL, -- Khóa ngoại
+	CONSTRAINT FK_VatNuoi_NguoiDung FOREIGN KEY (IDNguoiDung) REFERENCES NguoiDung(IDNguoiDung)
+);
+
+-- 4. Bảng Sức khỏe
+CREATE TABLE SucKhoe (
+    IDSucKhoe INT IDENTITY PRIMARY KEY,
+    IDVatNuoi VARCHAR(10) NOT NULL,
+    ngayKiemTra DATE NOT NULL,
+    tinhTrang NVARCHAR(200) NOT NULL,
+    ghiChu NVARCHAR(MAX),
+	CONSTRAINT FK_SucKhoe_VatNuoi FOREIGN KEY (IDVatNuoi) REFERENCES VatNuoi(IDVatNuoi)
+);
+
+-- 5. Bảng Kho
+CREATE TABLE Kho (
+    maKho INT IDENTITY PRIMARY KEY,
+    tenKho NVARCHAR(100) NOT NULL DEFAULT N'Chưa đặt tên',
+);
+
+-- 6. Bảng Thức ăn & nước uống
+CREATE TABLE ThucAnNuocUong (
+    IDThucAn INT NOT NULL PRIMARY KEY,
+    ten NVARCHAR(100) NOT NULL,
+    loai NVARCHAR(100) NOT NULL,
+    soLuong INT NOT NULL CHECK (soLuong >= 0),
+    hanSuDung DATE NOT NULL,
+	maKho INT NOT NULL,
+	CONSTRAINT FK_ThucAn_Kho FOREIGN KEY (maKho) REFERENCES Kho(maKho),
+);
+
+-- 7. Bảng phân phối thức ăn
+CREATE TABLE PhanPhoiThucAn (
+	ID INT IDENTITY PRIMARY KEY,
+    IDThucAn INT NOT NULL,
+    IDVatNuoi VARCHAR(10) NOT NULL,
+    ngay DATE NOT NULL,
+    soLuong FLOAT NOT NULL CHECK (soLuong > 0), --donvi kg
+	CONSTRAINT FK_PhanPhoiThucAn_ThucAnNuocUong FOREIGN KEY (IDThucAn) REFERENCES ThucAnNuocUong(IDThucAn) ON DELETE CASCADE,
+	CONSTRAINT FK_PhanPhoiThucAn_VatNuoi FOREIGN KEY (IDVatNuoi) REFERENCES VatNuoi(IDVatNuoi) ON DELETE CASCADE,
+);
+GO
+CREATE PROCEDURE USP_GetAccountByUserName
+@tenDangNhap NVARCHAR(50)
+AS
+BEGIN
+SELECT TenHienThi, Email FROM dbo.TaiKhoan WHERE TenDangNhap = @tenDangNhap
+END
---
+++ SQL file/THEMQLCN.sql
@@ -0,0 +1,17 @@
+INSERT INTO dbo.TaiKhoan(TenDangNhap, TenHienThi, MatKhau, Email, LoaiTaiKhoan)
+VALUES (N'tuank4',N'Minh Tuấn','2326','t@gmail.com','0')
+INSERT INTO dbo.TaiKhoan(TenDangNhap, TenHienThi, MatKhau, Email, LoaiTaiKhoan)
+VALUES (N'sangk3',N'Sang Sang','8386','Sa@gmail.com','1');
+INSERT INTO dbo.TaiKhoan(TenDangNhap, TenHienThi, MatKhau, Email, LoaiTaiKhoan)
+VALUES (N'khanhVQ',N'Khánh Vũ','1234','KVQ@gmail.com','0');
+INSERT INTO dbo.TaiKhoan(TenDangNhap, TenHienThi, MatKhau, Email, LoaiTaiKhoan)
+VALUES (N'LongHH',N'Long Hoàng','4567','Longp@gmail.com','0');
+
+INSERT INTO dbo.VatNuoi(IDVatNuoi, loai, tenGiong, tenChuong, gioitinh, ngaySinh, canNang)
+VALUES ('BO001', N'Bò', N'Simental', N'A1', N'Đực', '2022-05-01', 250.5);
+INSERT INTO dbo.VatNuoi (IDVatNuoi, loai, tenGiong, tenChuong, gioitinh, ngaySinh, canNang)
+VALUES ('LON001', N'Lợn', N'Landrace', N'B3', N'Cái', '2023-02-15', 120.0);
+INSERT INTO dbo.VatNuoi (IDVatNuoi, loai, tenGiong, tenChuong, gioitinh, ngaySinh, canNang)
+VALUES ('GA001', N'Gà', N'Gà Ri', N'C2', N'Đực', '2024-01-10', 2.3);
+INSERT INTO dbo.VatNuoi (IDVatNuoi, loai, tenGiong, tenChuong, gioitinh, ngaySinh, canNang)
+VALUES ('DE001', N'Dê', N'Bách Thảo', N'D1', N'Cái', '2023-09-20', 35.7);
