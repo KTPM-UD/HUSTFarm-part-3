2a7e87187812a5c994870400c5b9e4f92feda907
SangNmd2004 <158246508+SangNmd2004@users.noreply.github.com>
2025-05-26 12:58:39 +0700

    Add files via upload
    Hàm thêm vật nuôi

--- fAddLivestock.Designer.cs
+++ fAddLivestock.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class fAddLivestock
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,25 +15,27 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
+            textBox3 = new TextBox();
             textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
             button1 = new Button();
             button2 = new Button();
             dateTimePicker1 = new DateTimePicker();
             SuspendLayout();
             // 
-            // label1
+            // label1 - Loại
             // 
             label1.AutoSize = true;
             label1.Location = new Point(38, 61);
@@ -49,94 +44,131 @@
             label1.TabIndex = 0;
             label1.Text = "Loại";
             // 
-            // label2
+            // label2 - Giới tính
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(38, 159);
+            label2.Location = new Point(38, 139);
             label2.Name = "label2";
             label2.Size = new Size(156, 48);
             label2.TabIndex = 1;
             label2.Text = "Giới tính";
             // 
-            // label3
+            // label3 - Ngày sinh
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(38, 261);
+            label3.Location = new Point(38, 309);
             label3.Name = "label3";
             label3.Size = new Size(177, 48);
             label3.TabIndex = 2;
             label3.Text = "Ngày sinh";
             // 
-            // label4
+            // label4 - Cân nặng
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(38, 369);
+            label4.Location = new Point(38, 387);
             label4.Name = "label4";
             label4.Size = new Size(169, 48);
             label4.TabIndex = 3;
             label4.Text = "Cân nặng";
             // 
-            // textBox1
+            // label5 - Tên giống
             // 
+            label5.AutoSize = true;
+            label5.Location = new Point(38, 219);
+            label5.Name = "label5";
+            label5.Size = new Size(180, 48);
+            label5.TabIndex = 4;
+            label5.Text = "Tên giống";
+            // 
+            // label6 - ID vật nuôi
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(38, 465);
+            label6.Name = "label6";
+            label6.Size = new Size(182, 48);
+            label6.TabIndex = 5;
+            label6.Text = "ID vật nuôi";
+            // 
+            // label7 - Tên chuồng
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(38, 543);
+            label7.Name = "label7";
+            label7.Size = new Size(200, 48);
+            label7.TabIndex = 6;
+            label7.Text = "Tên chuồng";
+            // 
+            // textBox1 - Loại
             textBox1.Location = new Point(237, 61);
-            textBox1.Name = "textBox1";
             textBox1.Size = new Size(588, 55);
-            textBox1.TabIndex = 5;
+            textBox1.TabIndex = 7;
             // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(237, 159);
-            textBox2.Name = "textBox2";
+            // textBox2 - Giới tính
+            textBox2.Location = new Point(237, 139);
             textBox2.Size = new Size(588, 55);
-            textBox2.TabIndex = 6;
+            textBox2.TabIndex = 8;
             // 
-            // textBox4
+            // textBox3 - Tên giống
+            textBox3.Location = new Point(237, 219);
+            textBox3.Size = new Size(588, 55);
+            textBox3.TabIndex = 9;
             // 
-            textBox4.Location = new Point(237, 369);
-            textBox4.Name = "textBox4";
+            // textBox4 - Cân nặng
+            textBox4.Location = new Point(237, 387);
             textBox4.Size = new Size(588, 55);
-            textBox4.TabIndex = 8;
+            textBox4.TabIndex = 10;
             // 
-            // button1
+            // textBox5 - ID vật nuôi
+            textBox5.Location = new Point(237, 465);
+            textBox5.Size = new Size(588, 55);
+            textBox5.TabIndex = 11;
             // 
-            button1.Location = new Point(76, 503);
-            button1.Name = "button1";
+            // textBox6 - Tên chuồng
+            textBox6.Location = new Point(237, 543);
+            textBox6.Size = new Size(588, 55);
+            textBox6.TabIndex = 12;
+            // 
+            // dateTimePicker1 - Ngày sinh
+            dateTimePicker1.Location = new Point(237, 309);
+            dateTimePicker1.Size = new Size(588, 55);
+            dateTimePicker1.TabIndex = 13;
+            // 
+            // button1 - Lưu
+            button1.Location = new Point(76, 630);
             button1.Size = new Size(225, 69);
-            button1.TabIndex = 9;
             button1.Text = "Lưu";
+            button1.TabIndex = 14;
             button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            button2.Location = new Point(438, 503);
-            button2.Name = "button2";
+            // button2 - Hủy
+            button2.Location = new Point(438, 630);
             button2.Size = new Size(225, 69);
-            button2.TabIndex = 10;
             button2.Text = "Hủy";
+            button2.TabIndex = 15;
             button2.UseVisualStyleBackColor = true;
-            // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Location = new Point(237, 261);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(588, 55);
-            dateTimePicker1.TabIndex = 11;
+
             // 
             // fAddLivestock
             // 
             AutoScaleDimensions = new SizeF(20F, 48F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(877, 638);
-            Controls.Add(dateTimePicker1);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(textBox4);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
+            ClientSize = new Size(877, 750);
             Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(textBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox5);
+            Controls.Add(textBox6);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button1);
+            Controls.Add(button2);
             Name = "fAddLivestock";
             Text = "Form thêm vật nuôi";
             ResumeLayout(false);
@@ -149,9 +181,15 @@
         private Label label2;
         private Label label3;
         private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
         private TextBox textBox1;
         private TextBox textBox2;
+        private TextBox textBox3;
         private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
         private Button button1;
         private Button button2;
         private DateTimePicker dateTimePicker1;
--- fAddLivestock.cs
+++ fAddLivestock.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace HTQLCN
 {
@@ -15,6 +10,110 @@ namespace HTQLCN
         public fAddLivestock()
         {
             InitializeComponent();
+
+            // Gắn sự kiện cho nút Lưu
+            button1.Click += button1_Click;
+
+            // Gắn sự kiện khi thay đổi loại vật nuôi
+            textBox1.TextChanged += textBox1_TextChanged;
+        }
+
+        // Sự kiện khi bấm nút Lưu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string loai = textBox1.Text.Trim();
+            string gioiTinh = textBox2.Text.Trim();
+            string tenGiong = textBox3.Text.Trim();
+            DateTime ngaySinh = dateTimePicker1.Value;
+
+            if (!double.TryParse(textBox4.Text.Trim(), out double canNang))
+            {
+                MessageBox.Show("Cân nặng không hợp lệ!");
+                return;
+            }
+
+            string idMoi = textBox5.Text.Trim();
+            string tenChuong = textBox6.Text.Trim();
+
+            string connectionString = @"Data Source=ADMIN-PC;Initial Catalog=HeThongQuanLyChanNuoi;Integrated Security=True;Trust Server Certificate=True";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @"INSERT INTO dbo.VatNuoi(IDVatNuoi, loai, tenGiong, tenChuong, gioitinh, ngaySinh, canNang)
+                                 VALUES (@id, @loai, @tenGiong, @chuong, @gioiTinh, @ngaySinh, @canNang)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", idMoi);
+                cmd.Parameters.AddWithValue("@loai", loai);
+                cmd.Parameters.AddWithValue("@tenGiong", tenGiong);
+                cmd.Parameters.AddWithValue("@chuong", tenChuong);
+                cmd.Parameters.AddWithValue("@gioiTinh", gioiTinh);
+                cmd.Parameters.AddWithValue("@ngaySinh", ngaySinh);
+                cmd.Parameters.AddWithValue("@canNang", canNang);
+
+                int rows = cmd.ExecuteNonQuery();
+                MessageBox.Show(rows > 0 ? "Thêm thành công!" : "Thêm thất bại!");
+            }
+        }
+
+        // Khi thay đổi loại => cập nhật ID và tên chuồng
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string loai = textBox1.Text.Trim();
+            string prefix = GetPrefix(loai);
+            string idMoi = TaoIDMoi(prefix);
+            string chuong = GetChuong(loai);
+
+            textBox5.Text = idMoi;
+            textBox6.Text = chuong;
+        }
+
+        // Hàm sinh prefix ID theo loại
+        private string GetPrefix(string loai)
+        {
+            switch (loai.Trim().ToLower())
+            {
+                case "bò": return "BO";
+                case "lợn": return "LON";
+                case "gà": return "GA";
+                case "dê": return "DE";
+                default: return "XX";
+            }
+        }
+
+        // Hàm lấy tên chuồng theo loại
+        private string GetChuong(string loai)
+        {
+            switch (loai.Trim().ToLower())
+            {
+                case "bò": return "A1";
+                case "lợn": return "B3";
+                case "gà": return "C2";
+                case "dê": return "D1";
+                default: return "Z0";
+            }
+        }
+
+        // Hàm tạo ID mới dựa trên prefix
+        private string TaoIDMoi(string prefix)
+        {
+            string connectionString = @"Data Source=ADMIN-PC;Initial Catalog=HeThongQuanLyChanNuoi;Integrated Security=True;Trust Server Certificate=True";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = $"SELECT MAX(IDVatNuoi) FROM VatNuoi WHERE IDVatNuoi LIKE '{prefix}%'";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                object result = cmd.ExecuteScalar();
+
+                if (result == DBNull.Value || result == null)
+                {
+                    return prefix + "001";
+                }
+
+                string lastID = result.ToString();
+                int num = int.Parse(lastID.Substring(prefix.Length));
+                return prefix + (num + 1).ToString("D3");
+            }
         }
     }
 }
